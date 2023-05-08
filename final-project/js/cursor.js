ee59ab5f39fcd1979060cf8f9034f5945a750283
Miooo51532 <58099832+Miooo51532@users.noreply.github.com>
2023-05-08 21:09:05 +0800

    final project 1.1
    background & actress page update

--- final-project/js/cursor.js
+++ final-project/js/cursor.js
@@ -1,27 +1,101 @@
+// let cursorSketch;
+
+// function setup() {
+//     let canvas = createCanvas(windowWidth, windowHeight);
+//     canvas.parent("cursorcontainer");
+//     noCursor();
+//     cursorSketch = new CursorSketch();
+//     noStroke();
+// }
+
+// function draw() {
+//     background(226, 226, 226);
+//     cursorSketch.update(mouseX, mouseY);
+//     if(mouseIsPressed){
+//       cursorSketch.close();
+//     }else{
+//       cursorSketch.open();
+//     }
+// }
+
+// class CursorSketch {
+//     constructor() {
+//         this.x = 0;
+//         this.y = 0;
+//     }
+
+//     update(x, y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+
+//     open() {   
+//        // Draw the eye
+//        fill(255);
+//        ellipse(this.x, this.y, 40, 25);
+
+
+//        // Draw the pupil
+//        fill(0);
+//        ellipse(this.x, this.y, 12, 20);
+//     }
+
+//     close() {
+//         // Draw the eye
+//        fill(255);
+//        ellipse(this.x, this.y, 40, 10);
+
+//        // Draw the pupil
+//        fill(0);
+//        ellipse(this.x, this.y, 12, 10);
+//     }
+// }
+
 let cursorSketch;
+let eyebg = [];
 
 function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
-    canvas.parent("cursorcontainer");
+    // canvas.parent("cursorcontainer");
     noCursor();
-    cursorSketch = new CursorSketch();
+    cursorSketch = new CursorSketch(255, 1);
     noStroke();
+    for (let i = 0; i < 8; i++) {
+        eyebg[i] = new CursorSketch(100, random(1, 3));
+        eyebg[i].update(random(width), random(height));
+    }
 }
 
 function draw() {
     background(226, 226, 226);
+    for (let i = 0; i < 8; i++) {
+        // go to new positions
+        if (frameCount % 120 == 0) {
+            eyebg[i].update(random(width), random(height));
+        }
+
+        // blink
+        if (frameCount % 120 > 60 && frameCount % 120 < 80) {
+            eyebg[i].close();
+        } else {
+            eyebg[i].open();
+        }
+    }
+
     cursorSketch.update(mouseX, mouseY);
-    if(mouseIsPressed){
-      cursorSketch.close();
-    }else{
-      cursorSketch.open();
+    if (mouseIsPressed) {
+        cursorSketch.close();
+    } else {
+        cursorSketch.open();
     }
 }
 
 class CursorSketch {
-    constructor() {
+    constructor(t, s) {
         this.x = 0;
         this.y = 0;
+        this.trans = t;
+        this.s = s;
     }
 
     update(x, y) {
@@ -29,24 +103,23 @@ class CursorSketch {
         this.y = y;
     }
 
-    open() {   
-       // Draw the eye
-       fill(255);
-       ellipse(this.x, this.y, 40, 25);
-       
+    open() {
+        // Draw the eye
+        fill(255, 255, 255, this.trans);
+        ellipse(this.x, this.y, 40 * this.s, 25 * this.s);
 
-       // Draw the pupil
-       fill(0);
-       ellipse(this.x, this.y, 12, 20);
+        // Draw the pupil
+        fill(0, 0, 0, this.trans);
+        ellipse(this.x, this.y, 12 * this.s, 20 * this.s);
     }
 
     close() {
         // Draw the eye
-       fill(255);
-       ellipse(this.x, this.y, 40, 10);
-       
-       // Draw the pupil
-       fill(0);
-       ellipse(this.x, this.y, 12, 10);
+        fill(255, 255, 255, this.trans);
+        ellipse(this.x, this.y, 40 * this.s, 10 * this.s);
+
+        // Draw the pupil
+        fill(0, 0, 0, this.trans);
+        ellipse(this.x, this.y, 12 * this.s, 10 * this.s);
     }
 }
